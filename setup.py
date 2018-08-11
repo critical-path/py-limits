eca6dac320e8f16d7e76c25afd6f64fa43c577e0
critical-path <37241479+critical-path@users.noreply.github.com>
2018-08-11 16:41:13 -0400

    On branch master  Changes to be committed:
    	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import (
+    find_packages,
+    setup
+)
+
+
+setup(
+    name="py-limits",
+    version="0.1.0",
+    description="command-line util that retrieves rate limits from github",
+    url="https://github.com/critical-path/py-limits",
+    author="critical-path",
+    author_email="n/a",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 2 - Alpha",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ],
+    keywords="python command-line util rate limits github",
+    packages=find_packages(),
+    install_requires=[
+        "click",
+        "requests"
+    ],
+    extras_require={
+        "test": [
+            "coveralls",
+            "pytest",
+            "pytest-cov",
+            "responses"
+        ]
+    },
+    entry_points={
+        "console_scripts": [
+            "limits=limits.cli:limits"
+        ]
+    }
+)
